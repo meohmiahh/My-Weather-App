210cdd4a95ea34c47b62938e2bb67a76f68d259d
meohmiahh <mdiazaustin@gmail.com>
2021-11-24 16:53:31 -0600

    Altered weather forecast margin

--- src/index.js
+++ src/index.js
@@ -40,8 +40,11 @@ function displayWeatherCondition(response) {
   celsiusTemperature = response.data.main.temp;
   let iconElement = document.querySelector("#icon");
 
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", response.data.weather[0].description); 
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
