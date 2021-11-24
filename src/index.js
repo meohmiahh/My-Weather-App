90c8ad814daa863eb8e0eeb6a82b555faee76a72
meohmiahh <mdiazaustin@gmail.com>
2021-11-24 16:47:21 -0600

    Added relevant weather icon image

--- src/index.js
+++ src/index.js
@@ -38,6 +38,10 @@ function displayWeatherCondition(response) {
     response.data.weather[0].main;
 
   celsiusTemperature = response.data.main.temp;
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description); 
 }
 
 function searchCity(city) {
